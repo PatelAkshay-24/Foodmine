6fc9f0677dfdd6599ca0066eba49c9df70f0b26b
PatelAkshay-24 <akshay.patel@iflair.com>
2024-03-30 18:32:46 +0530

    patch update

--- frontend/src/app/components/partials/paypal-button/paypal-button.component.ts
+++ frontend/src/app/components/partials/paypal-button/paypal-button.component.ts
@@ -9,4 +9,58 @@ import { Order } from 'src/app/shared/models/Order';
 export class PaypalButtonComponent {
 @Input()
 order!:Order
+   
+constructor() { }
+handler:any = null;
+ngOnInit() {
+  this.loadStripe();
 }
+
+pay(amount: any) {    
+
+  var handler = (<any>window).StripeCheckout.configure({
+    key: 'pk_test_51Ozeq2SCAxCXHn3VrnMNDSRY83X0zDZnX0Oa8vjWp4v3NZcEHbF0qUN51xOjOZn7DQ5Xnv4e2mWC8QtH3GZxTc3G00NntabWhY',
+    locale: 'auto',
+    token: function (token: any) {
+      // You can access the token ID with `token.id`.
+      // Get the token ID to your server-side code for use.
+      console.log(token)
+      alert('Token Created!!');
+
+    }
+  });
+
+  handler.open({
+    name: 'Pyament Gateway',
+    description: 'Pay Your Food Bill',
+    amount: amount * 100
+  });
+
+}
+
+loadStripe() {
+   
+  if(!window.document.getElementById('stripe-script')) {
+    var s = window.document.createElement("script");
+    s.id = "stripe-script";
+    s.type = "text/javascript";
+    s.src = "https://checkout.stripe.com/checkout.js";
+    s.onload = () => {
+      this.handler = (<any>window).StripeCheckout.configure({
+        key: 'pk_test_51Ozeq2SCAxCXHn3VrnMNDSRY83X0zDZnX0Oa8vjWp4v3NZcEHbF0qUN51xOjOZn7DQ5Xnv4e2mWC8QtH3GZxTc3G00NntabWhY',
+        locale: 'auto',
+        token: function (token: any) {
+          // You can access the token ID with `token.id`.
+          // Get the token ID to your server-side code for use.
+          console.log(token)
+          alert('Payment Success!!');
+        }
+      });
+    }
+     
+    window.document.body.appendChild(s);
+  }
+}
+}
+
+
