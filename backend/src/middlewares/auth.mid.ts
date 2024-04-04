5d154d72fd678a7bfddd8d3c29574fc19b0a5512
PatelAkshay-24 <akshay.patel@iflair.com>
2024-04-04 12:49:55 +0530

    patch update

--- backend/src/middlewares/auth.mid.ts
+++ backend/src/middlewares/auth.mid.ts
@@ -1,52 +1,52 @@
-// //Taro code
+//Taro code
 
-// import { verify } from "jsonwebtoken";
-// import { HTTP_UNAUTHORIZED } from "../constant/http_status";
+import { verify } from "jsonwebtoken";
+import { HTTP_UNAUTHORIZED } from "../constant/http_status";
 
 
-// export default (req: any, res: any, next: any) => {
-//     const token = req.headers.access_token as string;
+export default (req: any, res: any, next: any) => {
+    const token = req.headers.access_token as string;
 
-//     if(!token) return res.status(HTTP_UNAUTHORIZED).send();
+    if(!token) return res.status(HTTP_UNAUTHORIZED).send();
    
     
-//     try {
-//         const decodedUser = verify(token, "SomeRandomText");
-//         req.user = decodedUser;
+    try {
+        const decodedUser = verify(token, "SomeRandomText");
+        req.user = decodedUser;
 
-//     } catch (error) {
-//         console.log("ppp"+token);
-//         res.status(HTTP_UNAUTHORIZED).send('please check');
-//     }
+    } catch (error) {
+        console.log("ppp"+token);
+        res.status(HTTP_UNAUTHORIZED).send('please check');
+    }
 
-//     return next();
-// }
+    return next();
+}
 
 
 
 //maro Code
 // auth.mid.js
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-const auth = (req:any, res:any, next:any) => {
-    const token = req.headers.authorization;
+// const auth = (req:any, res:any, next:any) => {
+//     const token = req.headers.authorization;
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized: Missing token' });
-    }
+//     if (!token) {
+//         return res.status(401).json({ message: 'Unauthorized: Missing token' });
+//     }
 
-    try {
-        const decoded = jwt.verify(token.split(' ')[1], "SomeRandomText");
-        console.log('Decoded Token:', decoded); // Log decoded token for debugging
-        req.user = decoded;
-        next();
-    } catch (err) {
-        console.error('Token Verification Error:', err); // Log token verification errors
-        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
-    }
-};
+//     try {
+//         const decoded = jwt.verify(token.split(' ')[1], "SomeRandomText");
+//         console.log('Decoded Token:', decoded); // Log decoded token for debugging
+//         req.user = decoded;
+//         next();
+//     } catch (err) {
+//         console.error('Token Verification Error:', err); // Log token verification errors
+//         return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+//     }
+// };
 
-export default auth;
+// export default auth;
 
 
 
